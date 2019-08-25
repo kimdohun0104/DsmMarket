4b4ca24f7dde8cd7fc1a372b1b0bbbe967062b26
Kimdohun <dikolight203@gmail.com>
2019-08-26 08:29:54 +0900

    jenkins test

--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dsm.dsmmarketandroid.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.dsm.dsmmarketandroid.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("DEBUGLOG", "jenkins test")
     }
 }
