cc06624ef14d23740123349d9100735f43c57f0c
Kimdohun <dikolight203@gmail.com>
2019-08-29 15:33:42 +0900

    some

--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/post/postRent/PostRentActivity.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/post/postRent/PostRentActivity.kt
@@ -3,11 +3,18 @@ package com.dsm.dsmmarketandroid.presentation.ui.post.postRent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dsm.dsmmarketandroid.R
+import com.dsm.dsmmarketandroid.presentation.ui.category.CategoryActivity
+import com.dsm.dsmmarketandroid.presentation.ui.postCategory.PostCategoryActivity
+import kotlinx.android.synthetic.main.activity_post_rent.*
+import org.jetbrains.anko.startActivity
 
 class PostRentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_rent)
+        tb_post_rent.setNavigationOnClickListener { finish() }
+
+        cl_category.setOnClickListener { startActivity<PostCategoryActivity>() }
     }
 }
---
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/postCategory/PostCategoryActivity.kt
@@ -0,0 +1,13 @@
+package com.dsm.dsmmarketandroid.presentation.ui.postCategory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dsm.dsmmarketandroid.R
+
+class PostCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_category)
+    }
+}
