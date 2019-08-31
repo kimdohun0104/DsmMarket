113dc61a7d39cc5d3a8c7b7ae8bb5c6e734085b8
Kimdohun <dikolight203@gmail.com>
2019-08-31 11:23:57 +0900

    ChangePassword

--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/changePassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/changePassword/ChangePasswordViewModel.kt
@@ -1,11 +1,60 @@
 package com.dsm.dsmmarketandroid.presentation.ui.password.changePassword
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.dsm.domain.usecase.ChangePasswordUseCase
 import com.dsm.dsmmarketandroid.presentation.base.BaseViewModel
+import com.dsm.dsmmarketandroid.presentation.util.SingleLiveEvent
 
 class ChangePasswordViewModel(
     private val changePasswordUseCase: ChangePasswordUseCase
 ) : BaseViewModel() {
 
+    val newPassword = MutableLiveData<String>()
+    val reType = MutableLiveData<String>()
 
+    val isChangePasswordEnable = MediatorLiveData<Boolean>().apply {
+        addSource(newPassword) { value = !newPassword.value.isNullOrBlank() && !reType.value.isNullOrBlank() }
+        addSource(reType) { value = !newPassword.value.isNullOrBlank() && !reType.value.isNullOrBlank() }
+    }
+
+    val passwordDiffEvent = SingleLiveEvent<Any>()
+    val serverErrorEvent = SingleLiveEvent<Any>()
+    val changePasswordFailEvent = SingleLiveEvent<Any>()
+
+    val finishActivityEvent = SingleLiveEvent<Any>()
+
+    val intentLoginActivity = SingleLiveEvent<Any>()
+
+
+    fun changePassword(email: String) {
+        if (newPassword.value != reType.value) {
+            passwordDiffEvent.call()
+            return
+        }
+
+        addDisposable(
+            if (email.isBlank()) {
+                changePasswordUseCase.create(newPassword.value!!)
+                    .subscribe({
+                        when (it.code()) {
+                            200 -> finishActivityEvent.call()
+                            else -> changePasswordFailEvent.call()
+                        }
+                    }, {
+                        serverErrorEvent.call()
+                    })
+            } else {
+                changePasswordUseCase.create(ChangePasswordUseCase.Params(email, newPassword.value!!))
+                    .subscribe({
+                        when (it.code()) {
+                            200 -> intentLoginActivity.call()
+                            else -> changePasswordFailEvent.call()
+                        }
+                    }, {
+                        serverErrorEvent.call()
+                    })
+            }
+        )
+    }
 }
--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/util/BindingUtil.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/util/BindingUtil.kt
@@ -3,6 +3,10 @@ package com.dsm.dsmmarketandroid.presentation.util
 import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
+import android.graphics.drawable.ColorDrawable
+import androidx.databinding.BindingConversion
+
+
 
 object BindingUtil {
 
@@ -10,5 +14,11 @@ object BindingUtil {
     @BindingAdapter("bind:spinner")
     fun bindSpn(view: Spinner, value: MutableLiveData<String>) {
         value.value = view.selectedItem.toString()
+    }
+
+    @JvmStatic
+    @BindingConversion
+    fun convertColorToDrawable(color: Int): ColorDrawable? {
+        return if (color != 0) ColorDrawable(color) else null
     }
 }
