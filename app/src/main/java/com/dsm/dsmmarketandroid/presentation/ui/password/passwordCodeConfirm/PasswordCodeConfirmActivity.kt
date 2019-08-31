5ea6edcccce89c0f4b015a490732f346edc5638c
Kimdohun <dikolight203@gmail.com>
2019-08-31 10:43:00 +0900

    PasswordCodeConfirm

--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/passwordCodeConfirm/PasswordCodeConfirmActivity.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/passwordCodeConfirm/PasswordCodeConfirmActivity.kt
@@ -1,10 +1,14 @@
 package com.dsm.dsmmarketandroid.presentation.ui.password.passwordCodeConfirm
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.dsm.dsmmarketandroid.R
 import com.dsm.dsmmarketandroid.databinding.ActivityPasswordCodeConfirmBinding
 import com.dsm.dsmmarketandroid.presentation.ui.base.BaseActivity
+import com.dsm.dsmmarketandroid.presentation.ui.password.changePassword.ChangePasswordActivity
 import kotlinx.android.synthetic.main.activity_password_code_confirm.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PasswordCodeConfirmActivity : BaseActivity<ActivityPasswordCodeConfirmBinding>() {
@@ -16,6 +20,16 @@ class PasswordCodeConfirmActivity : BaseActivity<ActivityPasswordCodeConfirmBind
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tb_confirm_code.setNavigationOnClickListener { finish() }
+        val email = intent.getStringExtra("email")
+        binding.email = email
+
+        viewModel.intentChangePasswordWithEmail.observe(this, Observer {
+            startActivity<ChangePasswordActivity>("email" to email)
+        })
+
+        viewModel.confirmCodeFailEvent.observe(this, Observer { toast(R.string.fail_confirm_code) })
+
+        viewModel.serverErrorEvent.observe(this, Observer { toast(R.string.fail_server_error) })
 
         binding.viewModel = viewModel
     }
--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/sendPasswordCode/SendPasswordCodeViewModel.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/password/sendPasswordCode/SendPasswordCodeViewModel.kt
@@ -1,6 +1,5 @@
 package com.dsm.dsmmarketandroid.presentation.ui.password.sendPasswordCode
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
