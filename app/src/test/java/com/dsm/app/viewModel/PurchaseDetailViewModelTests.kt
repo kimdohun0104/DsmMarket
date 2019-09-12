51ebd81bcf4df1b4f43fe87650d36d3246071626
kimdohun0104 <dikolight203@gmail.com>
2019-09-12 21:01:31 +0900

    test: purchase / rent view model test case about interest

--- app/src/test/java/com/dsm/app/viewModel/PurchaseDetailViewModelTests.kt
+++ app/src/test/java/com/dsm/app/viewModel/PurchaseDetailViewModelTests.kt
@@ -3,10 +3,14 @@ package com.dsm.app.viewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.dsm.domain.entity.PurchaseDetail
 import com.dsm.domain.usecase.GetPurchaseDetailUseCase
+import com.dsm.domain.usecase.InterestUseCase
+import com.dsm.domain.usecase.UnInterestUseCase
 import com.dsm.dsmmarketandroid.presentation.mapper.PurchaseDetailModelMapper
 import com.dsm.dsmmarketandroid.presentation.ui.purchaseDetail.PurchaseDetailViewModel
 import com.jraska.livedata.test
 import io.reactivex.Flowable
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -20,14 +24,18 @@ class PurchaseDetailViewModelTests {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var getPurchaseDetailUseCase: GetPurchaseDetailUseCase
+    private lateinit var interestUseCase: InterestUseCase
+    private lateinit var unInterestUseCase: UnInterestUseCase
     private lateinit var purchaseDetailModelMapper: PurchaseDetailModelMapper
     private lateinit var viewModel: PurchaseDetailViewModel
 
     @Before
     fun init() {
         getPurchaseDetailUseCase = mock(GetPurchaseDetailUseCase::class.java)
+        interestUseCase = mock(InterestUseCase::class.java)
+        unInterestUseCase = mock(UnInterestUseCase::class.java)
         purchaseDetailModelMapper = PurchaseDetailModelMapper()
-        viewModel = PurchaseDetailViewModel(getPurchaseDetailUseCase, purchaseDetailModelMapper)
+        viewModel = PurchaseDetailViewModel(getPurchaseDetailUseCase, interestUseCase, unInterestUseCase, purchaseDetailModelMapper)
     }
 
     @Test
@@ -40,7 +48,8 @@ class PurchaseDetailViewModelTests {
             title = "TITLE",
             commentCount = 0,
             content = "CONTENT",
-            img = listOf("IMAGE1", "asdf")
+            img = listOf("IMAGE1", "asdf"),
+            isInterest = true
         )
         `when`(getPurchaseDetailUseCase.create(0))
             .thenReturn(Flowable.just(response))
@@ -50,5 +59,31 @@ class PurchaseDetailViewModelTests {
         val result = purchaseDetailModelMapper.mapFrom(response)
 
         viewModel.purchaseDetail.test().assertValue(result)
+    }
+
+    @Test
+    fun `interest purchase success`() {
+        viewModel.isInterest.value = false
+
+        `when`(interestUseCase.create(InterestUseCase.Params(0, 0)))
+            .thenReturn(Flowable.just(Unit))
+
+        viewModel.onClickInterest(0)
+
+        assertTrue(viewModel.isInterest.test().value())
+        viewModel.toastInterestEvent.test().assertHasValue()
+    }
+
+    @Test
+    fun `un interest purchase success`() {
+        viewModel.isInterest.value = true
+
+        `when`(unInterestUseCase.create(UnInterestUseCase.Params(0, 0)))
+            .thenReturn(Flowable.just(Unit))
+
+        viewModel.onClickInterest(0)
+
+        assertFalse(viewModel.isInterest.test().value())
+        viewModel.toastUnInterestEvent.test().assertHasValue()
     }
 }
--- app/src/test/java/com/dsm/app/viewModel/RentDetailViewModelTests.kt
+++ app/src/test/java/com/dsm/app/viewModel/RentDetailViewModelTests.kt
@@ -3,10 +3,14 @@ package com.dsm.app.viewModel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.dsm.domain.entity.RentDetail
 import com.dsm.domain.usecase.GetRentDetailUseCase
+import com.dsm.domain.usecase.InterestUseCase
+import com.dsm.domain.usecase.UnInterestUseCase
 import com.dsm.dsmmarketandroid.presentation.mapper.RentDetailModelMapper
 import com.dsm.dsmmarketandroid.presentation.ui.rentDetail.RentDetailViewModel
 import com.jraska.livedata.test
 import io.reactivex.Flowable
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -20,14 +24,18 @@ class RentDetailViewModelTests {
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var getRentDetailUseCase: GetRentDetailUseCase
+    private lateinit var interestUseCase: InterestUseCase
+    private lateinit var unInterestUseCase: UnInterestUseCase
     private lateinit var rentDetailModelMapper: RentDetailModelMapper
     private lateinit var viewModel: RentDetailViewModel
 
     @Before
     fun init() {
         getRentDetailUseCase = mock(GetRentDetailUseCase::class.java)
+        interestUseCase = mock(InterestUseCase::class.java)
+        unInterestUseCase = mock(UnInterestUseCase::class.java)
         rentDetailModelMapper = RentDetailModelMapper()
-        viewModel = RentDetailViewModel(getRentDetailUseCase, rentDetailModelMapper)
+        viewModel = RentDetailViewModel(getRentDetailUseCase, interestUseCase, unInterestUseCase, rentDetailModelMapper)
     }
 
     @Test
@@ -41,7 +49,8 @@ class RentDetailViewModelTests {
             content = "CONTENT",
             title = "TITLE",
             author = "AUTHOR",
-            img = "IMG"
+            img = "IMG",
+            isInterest = true
         )
         `when`(getRentDetailUseCase.create(0)).thenReturn(Flowable.just(response))
 
@@ -50,5 +59,31 @@ class RentDetailViewModelTests {
         val result = rentDetailModelMapper.mapFrom(response)
 
         viewModel.rentDetail.test().assertValue(result)
+    }
+
+    @Test
+    fun interestRentSuccess() {
+        viewModel.isInterest.value = false
+
+        `when`(interestUseCase.create(InterestUseCase.Params(0, 1)))
+            .thenReturn(Flowable.just(Unit))
+
+        viewModel.onClickInterest(0)
+
+        assertTrue(viewModel.isInterest.test().value())
+        viewModel.toastInterestEvent.test().assertHasValue()
+    }
+
+    @Test
+    fun unInterestRentSuccess() {
+        viewModel.isInterest.value = true
+
+        `when`(unInterestUseCase.create(UnInterestUseCase.Params(0, 1)))
+            .thenReturn(Flowable.just(Unit))
+
+        viewModel.onClickInterest(0)
+
+        assertFalse(viewModel.isInterest.test().value())
+        viewModel.toastUnInterestEvent.test().assertHasValue()
     }
 }
