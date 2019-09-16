ef068da62cf73761371c9d96c893401d3e1e5e38
kimdohun0104 <dikolight203@gmail.com>
2019-09-16 21:45:32 +0900

    test: interesting view model test cases added

---
+++ app/src/test/java/com/dsm/app/viewModel/InterestViewModelTests.kt
@@ -0,0 +1,62 @@
+package com.dsm.app.viewModel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.dsm.domain.entity.Product
+import com.dsm.domain.usecase.GetInterestUseCase
+import com.dsm.dsmmarketandroid.presentation.mapper.ProductModelMapper
+import com.dsm.dsmmarketandroid.presentation.ui.interest.InterestViewModel
+import com.jraska.livedata.test
+import io.reactivex.Flowable
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+class InterestViewModelTests {
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var getInterestUseCase: GetInterestUseCase
+    private lateinit var productModelMapper: ProductModelMapper
+    private lateinit var viewModel: InterestViewModel
+
+    @Before
+    fun init() {
+        getInterestUseCase = mock(GetInterestUseCase::class.java)
+        productModelMapper = ProductModelMapper()
+        viewModel = InterestViewModel(getInterestUseCase, productModelMapper)
+    }
+
+    @Test
+    fun getInterestPurchase() {
+        val response = listOf(Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"))
+        `when`(getInterestUseCase.create(0))
+            .thenReturn(Flowable.just(response))
+
+        viewModel.getInterestPurchase()
+
+        viewModel.purchaseList.test().assertValue(productModelMapper.mapFrom(response))
+    }
+
+    @Test
+    fun getInterestRent() {
+        val response = listOf(
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE"),
+            Product(0, "TITLE", "IMG", "CREATED_AT", "PRICE")
+        )
+        `when`(getInterestUseCase.create(1))
+            .thenReturn(Flowable.just(response))
+
+        viewModel.getInterestRent()
+
+        viewModel.rentList.test().assertValue(productModelMapper.mapFrom(response))
+    }
+}
