4c37108635a7a5ebed5a26d6b26af46bd0b09794
kimdohun0104 <dikolight203@gmail.com>
2019-11-04 11:26:09 +0900

    change BottomSheet to Dialog Bottom sheet의 배경색이 제대로 설정되지 않는 문제 때문에 Dialog로 변경함

--- app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/modify/rent/rentTime/ModifyRentTimeDialog.kt
+++ app/src/main/java/com/dsm/dsmmarketandroid/presentation/ui/modify/rent/rentTime/ModifyRentTimeDialog.kt
@@ -1,24 +1,24 @@
-package com.dsm.dsmmarketandroid.presentation.ui.post.postRent.rentTime
+package com.dsm.dsmmarketandroid.presentation.ui.modify.rent.rentTime
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.dsm.dsmmarketandroid.R
-import com.dsm.dsmmarketandroid.presentation.ui.adapter.RentTimePagerAdapter
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.dsm.dsmmarketandroid.presentation.ui.adapter.ModifyRentTimePagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.fragment_select_rent_time.*
+import kotlinx.android.synthetic.main.dialog_select_rent_time.*
 
-class SelectRentTimeFragment : BottomSheetDialogFragment() {
+class ModifyRentTimeDialog : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_select_rent_time, container, false)
+        inflater.inflate(R.layout.dialog_select_rent_time, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        vp_rent_time.adapter = RentTimePagerAdapter(childFragmentManager, lifecycle)
+        vp_rent_time.adapter = ModifyRentTimePagerAdapter(childFragmentManager, lifecycle)
         TabLayoutMediator(tl_rent_time, vp_rent_time, true) { tab, position ->
             when (position) {
                 0 -> tab.text = getString(R.string.start)
