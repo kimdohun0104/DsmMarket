d96f3724a368a06efcb858120566f83c3d652b38
kimdohun0104 <dikolight203@gmail.com>
2019-11-18 23:18:54 +0900

    [TEST] TestCode updated with okhttp

--- app/src/test/java/com/dsm/app/TestHelper.kt
+++ app/src/test/java/com/dsm/app/TestHelper.kt
@@ -1,20 +1,24 @@
 package com.dsm.app
 
-import okhttp3.MediaType
-import okhttp3.ResponseBody
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.HttpException
 import retrofit2.Response
 
 fun createHttpException(errorCode: Int): HttpException {
-    val errorResponse: Response<Unit> = Response.error(errorCode,
-        ResponseBody.create(MediaType.parse("application/json"), ""))
+    val errorResponse: Response<Unit> = Response.error(
+        errorCode,
+        "".toResponseBody("application/json".toMediaTypeOrNull())
+    )
 
     return HttpException(errorResponse)
 }
 
 fun createHttpException(errorCode: Int, json: String): HttpException {
-    val errorResponse: Response<Unit> = Response.error(errorCode,
-        ResponseBody.create(MediaType.parse("application/json; charset=utf-8"), json))
+    val errorResponse: Response<Unit> = Response.error(
+        errorCode,
+        json.toResponseBody("application/json; charset=utf-8".toMediaTypeOrNull())
+    )
 
     return HttpException(errorResponse)
 }
